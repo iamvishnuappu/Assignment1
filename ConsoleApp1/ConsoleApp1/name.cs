2bf0088499b57d1da92cc99dfeb4b0bef7ffe250
Vishnu Mohan <734906@student.tor.loc>
2019-05-29 10:32:44 -0400

    initial

---
+++ ConsoleApp1/ConsoleApp1/name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello");
+            Console.WriteLine("My name:" + name);
+            Console.ReadKey();
+        }
+    }
+}
