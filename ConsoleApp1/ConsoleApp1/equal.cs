8faad82c0714bda3a798dfc007c494ec1a469325
Vishnu Mohan <734906@student.tor.loc>
2019-05-29 12:01:16 -0400

    final

--- ConsoleApp1/ConsoleApp1/equal.cs
+++ ConsoleApp1/ConsoleApp1/equal.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp4
 {
     class Program
     {
@@ -20,17 +20,20 @@ namespace ConsoleApp1
 
         public void calc()
         {
-            int n1, n2;
+            int n1, n2,n3;
 
             Console.WriteLine("Enter two number");
             n1 = Convert.ToInt32(Console.ReadLine());
             n2 = Convert.ToInt32(Console.ReadLine());
-            if (n1==n2)
+            n3 = Convert.ToInt32(Console.ReadLine());
+            if ((n1 >n2)&&(n1>n3))
             {
-                Console.WriteLine("Numbers are equal");
+                Console.WriteLine(n1+" is greater");
             }
+            else if((n2 > n1) && (n2 > n3))
+                Console.WriteLine(n2 + " is greater");
             else
-                Console.WriteLine("Numbers are not equal");
+                Console.WriteLine(n3 + " is greater");
             Console.ReadKey();
         }
     }
